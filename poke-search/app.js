922a56f1b5f5352772029c3ac57940e0c59e82a5
Chrawnus <65030578+Chrawnus@users.noreply.github.com>
2020-10-02 13:57:11 +0300

    update pokésearch
    Basic search functionality added.
    
    Updated CSS styling.

--- poke-search/app.js
+++ poke-search/app.js
@@ -1,15 +1,45 @@
+const searchBarElem = document.getElementById('search-bar');
+const searchButtonElem = document.getElementById('search-button');
+
 const wrapper = document.getElementById('wrapper');
-const name = document.getElementById('name');
+const nameElem = document.getElementById('name');
+const weightElem = document.getElementById('weight');
+const heightElem = document.getElementById('height');
+const typeElem = document.getElementById('type');
+const frontImgElem = document.getElementById('front-sprite');
+const backImgElem = document.getElementById('back-sprite');
 
-
-fetch('https://pokeapi.co/api/v2/pokemon/ditto')
-.then(response => response.json())
-.then(onLoad);
+searchButtonElem.addEventListener('click', getInfo);
+searchBarElem.addEventListener("keyup", function (e) { if (e.key === "Enter") {
+    e.preventDefault();
+    searchButtonElem.click();
+    }
+  });
 
 
 function onLoad(pokeInfo) {
-    console.log(pokeInfo);
-
-    name.textContent += pokeInfo.name;
     
+    const sprites = pokeInfo.sprites;
+
+    console.log(sprites.back_default);
+
+    let types = pokeInfo.types.flat(Infinity).map( x => x = x["type"]["name"]).join("/");
+
+    
+    nameElem.textContent = `Name: ${pokeInfo.name.slice(0,1).toUpperCase() + pokeInfo.name.slice(1, pokeInfo.name.length)}`;
+    heightElem.textContent = `Height: ${pokeInfo.height} dm`;
+    weightElem.textContent = `Weight: ${pokeInfo.weight} hg`;
+    typeElem.textContent = `Type(s): ${types}`;
+    frontImgElem.setAttribute('src', sprites.front_default);
+    backImgElem.setAttribute('src', sprites.back_default);
+}
+
+function getInfo() {
+
+        fetch(`https://pokeapi.co/api/v2/pokemon/${searchBarElem.value.toLowerCase()}`)
+        .then( response => response.json())
+        .then(onLoad)
+        .catch(() => alert('Please enter valid pokémon name!'));
+        
+
 }
