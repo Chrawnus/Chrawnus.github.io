0f766b7a69285d5c50de73b831f7c204dd0104cf
Chrawnus <65030578+Chrawnus@users.noreply.github.com>
2021-02-12 21:20:23 +0200

    Made path/node system less complicated

--- pathfinding/scripts/TileGrid.js
+++ pathfinding/scripts/TileGrid.js
@@ -12,10 +12,9 @@ export class TileGrid {
             height: canvasElem.height / (Math.sqrt(this.tiles))
         }
         this.startNode;
-        this.lastStartNode = [];
         this.nodes = [];
-        this.helper = new HelperFunctions();
         this.paths = [];
+        this.helper = new HelperFunctions();
     }
 
     addWorld(obj) {
@@ -37,15 +36,12 @@ export class TileGrid {
                         "y": y,
                         "traversable": false,
                         "node": false,
-                        "startNode": false,
-                        "parentNode": undefined,
-                        "childNodes": [
+                        "connectingNodes": [
                             undefined,
                             undefined,
                             undefined,
                             undefined
-                        ],
-                        "endPoint": false
+                        ]
                     });
             }
         }
@@ -59,16 +55,11 @@ export class TileGrid {
             } else if (grid[i].traversable === true && grid[i].node === false) {
                 this.drawTile(ctx, grid, i, `white`);
             }
-            if (grid[i].node === true && !(grid[i].startNode)) {
+            if (grid[i].node === true) {
                 this.drawTile(ctx, grid, i, `black`);
             }
-            if (grid[i].startNode && this.startNode === grid[i]) {
+            if (this.startNode === grid[i]) {
                 this.drawTile(ctx, grid, i, `red`);
-            } else if (grid[i].startNode && this.startNode !== grid[i]) {
-                this.drawTile(ctx, grid, i, `pink`);
-            } 
-            if (grid[i].endPoint && this.startNode !== grid[i]) {
-                this.drawTile(ctx, grid, i, `green`);
             }
         }
     }
@@ -84,124 +75,110 @@ export class TileGrid {
         const tileWidth = this.tileSize.width;
         const tileHeight = this.tileSize.height;
         const nodes = this.nodes;
-
         for (let i = 0; i < grid.length; i++) {
             if (isPointOnTile(cursorX, grid, i, tileWidth, cursorY, tileHeight)) {
                 if (keyArr.includes("Control")) {
                     if (nodes.length === 1 && nodes.includes(grid[i])) { // remove first node
-                        this.removeNode(grid, i, nodes, 0, this.paths);
+                        this.removeNode(grid, i, nodes, 0);
                     } else if (nodes.includes(grid[i])) {
-                        this.removeNode(grid, i, nodes, 0, this.paths);
+                        this.removeNode(grid, i, nodes, 0);
                     }
                 } else if (!(keyArr.includes("Control"))) {
-                    if (this.startNode === undefined && grid[i].startNode === false && nodes.length === 0) { // create first node
+                    if (nodes.length === 0) { // create first node
                         this.addNode(grid, i, nodes);
                     } else if (this.startNode !== grid[i] && !(nodes.includes(grid[i])) && arePointsOnCoordinateLines(this.startNode, grid, i)) {
                         this.addIfFreeChildNode(grid, i, nodes);
-                        this.updatePath();
                     } else if (nodes.includes(grid[i])) {
-                        grid[i].startNode = true;
-                        this.startNode = grid[i];
-                        this.removePrevStartNode(grid, i);
-                        console.log(grid[i])
+                        if (keyArr.includes("Shift")) {
+                            this.connectNodes(grid, i);
+                        } else {
+                            this.startNode = grid[i];
+                        }
                     }
                 }
+                this.updatePath();
             }
         }
         console.log(nodes);
     }
 
-    addIfFreeChildNode(grid, i, nodes) {
-        let tile = grid[i];
-        let parent = tile.parentNode;
-        if (tile.y < this.startNode.y && this.startNode.childNodes[0] === undefined) {
-            this.addNode(grid, i, nodes);
-        } else if (tile.x > this.startNode.x && this.startNode.childNodes[1] === undefined) {
-            this.addNode(grid, i, nodes);
-        } else if (tile.y > this.startNode.y && this.startNode.childNodes[2] === undefined) {
-            this.addNode(grid, i, nodes);
-        } else if (tile.x < this.startNode.x && this.startNode.childNodes[3] === undefined) {
-            this.addNode(grid, i, nodes);
+    connectNodes(grid, i) {
+        if (this.startNode.y > grid[i].y && grid[i].connectingNodes[2] === undefined && this.startNode.connectingNodes[0] === undefined) {
+            this.startNode.connectingNodes[0] = grid[i];
+            grid[i].connectingNodes[2] = this.startNode;
+        }
+        if (this.startNode.x < grid[i].x && grid[i].connectingNodes[3] === undefined && this.startNode.connectingNodes[1] === undefined) {
+            this.startNode.connectingNodes[1] = grid[i];
+            grid[i].connectingNodes[3] = this.startNode;
+        }
+        if (this.startNode.y < grid[i].y && grid[i].connectingNodes[0] === undefined && this.startNode.connectingNodes[2] === undefined) {
+            this.startNode.connectingNodes[2] = grid[i];
+            grid[i].connectingNodes[0] = this.startNode;
+        }
+        if (this.startNode.x > grid[i].x && grid[i].connectingNodes[1] === undefined && this.startNode.connectingNodes[3] === undefined) {
+            this.startNode.connectingNodes[3] = grid[i];
+            grid[i].connectingNodes[1] = this.startNode;
         }
     }
 
-    removePrevStartNode(grid, i) {
-        for (let j = 0; j < grid[i].childNodes.length; j++) {
-            const element = grid[i].childNodes[j];
-            if (element !== undefined && this.checkForchildNodes(grid, grid.indexOf(element))) {
-                element.startNode = false;
-            }
+    addIfFreeChildNode(grid, i, nodes) {
+        if (grid[i].y < this.startNode.y && this.startNode.connectingNodes[0] === undefined) {
+            this.addNode(grid, i, nodes);
+        } else if (grid[i].x > this.startNode.x && this.startNode.connectingNodes[1] === undefined) {
+            this.addNode(grid, i, nodes);
+        } else if (grid[i].y > this.startNode.y && this.startNode.connectingNodes[2] === undefined) {
+            this.addNode(grid, i, nodes);
+        } else if (grid[i].x < this.startNode.x && this.startNode.connectingNodes[3] === undefined) {
+            this.addNode(grid, i, nodes);
         }
     }
 
     addNode(grid, i, nodes) {
         let tile = grid[i];
-        let parent = this.startNode;
-
         if (this.startNode === undefined) {
-            tile.startNode = true;
             this.startNode = grid[i];
         }
-
         this.flipNode(grid, i);
         nodes.push(tile);
-        tile.parentNode = parent;
-        if (tile.parentNode !== undefined) {
-            this.appendChildNode(grid, i, this.startNode);
+        this.appendChildNode(grid, i, this.startNode);
+    }
+
+    removeNode(grid, i, nodes) {
+        let tile = grid[i];
+        let paths = this.paths;
+        tile.node = false;
+        tile.traversable = false;
+        this.clearConnectingNodes(tile, nodes)
+        nodes.splice(nodes.indexOf(tile), 1);
+        if (this.startNode === tile) {
+            this.startNode = nodes[nodes.length - 1];
+        }
+        for (let k = 0; k < paths.length; k++) {
+            const path = paths[k];
+            if (path.includes(tile)) {
+                for (let l = 0; l < path.length; l++) {
+                    path[l].traversable = false;
+                }
+            }
         }
     }
 
-    removeNode(grid, i, nodes, depth, paths) {
-        console.log(depth)
-        
-        if (this.checkNumberOfCnNodes(grid, i) === 0 && depth === 0) {
-            if (paths.length > 0) {
-     
-
-            }
-            grid[i].traversable = false;
-            grid[i].node = false;
-            grid[i].startNode = false;
-            grid[i].endPoint = false;
-            if (grid[i].parentNode === undefined) {
-                console.log("removing startnode")
-                this.startNode = undefined;
-            } else {
-                this.startNode = grid[i].parentNode;
-            }
-
-            return nodes.splice(nodes.indexOf(grid[i]), 1);
-        }
-
-        if (this.checkNumberOfCnNodes(grid, i) === 0 && depth > 0) {
-
-            let parent = grid[i].parentNode
-            this.startNode = parent;
-            this.deleteChildNode(grid, i, grid[i].parentNode);
-            grid[i].startNode = false;
-            if (grid[i].node) {
-                nodes.splice(nodes.indexOf(grid[i]), 1);
-                grid[i].traversable = false;
-                grid[i].node = false;
-                grid[i].endPoint = false;
-            }
-
-            return this.removeNode(grid, grid.indexOf(parent), nodes, depth -= 1, paths);
-        }
-
-        if (this.checkNumberOfCnNodes(grid, i) > 0) {
-            let cNode = grid[i].childNodes;
-            if (cNode[0] !== undefined) {
-                this.removeNode(grid, grid.indexOf(cNode[0]), nodes, depth += 1, paths);
-            }
-            if (cNode[1] !== undefined) {
-                this.removeNode(grid, grid.indexOf(cNode[1]), nodes, depth += 1, paths);
-            }
-            if (cNode[2] !== undefined) {
-                this.removeNode(grid, grid.indexOf(cNode[2]), nodes, depth += 1, paths);
-            }
-            if (cNode[3] !== undefined) {
-                this.removeNode(grid, grid.indexOf(cNode[3]), nodes, depth += 1, paths);
+    clearConnectingNodes(tile, nodes) {
+        for (let i = 0; i < tile.connectingNodes.length; i++) {
+            let connectedNode = tile.connectingNodes[i];
+            if (connectedNode !== undefined) {
+                tile.connectingNodes[i] = undefined;
+                for (let j = 0; j < connectedNode.connectingNodes.length; j++) {
+                    if (connectedNode.connectingNodes[j] === tile) {
+                        connectedNode.connectingNodes[j] = undefined;
+                        if (this.checkForConnectingNodes(connectedNode)) {
+                            console.log("removing: ", connectedNode)
+                            connectedNode.node = false;
+                            connectedNode.traversable = false;
+                            nodes.splice(nodes.indexOf(connectedNode), 1);
+                        }
+                    }
+                }
             }
         }
     }
@@ -214,63 +191,49 @@ export class TileGrid {
         let west = 3;
         if (tile !== startNode) {
             if (tile.y < startNode.y) {
-                return startNode.childNodes[north] = tile;
+                startNode.connectingNodes[north] = tile;
+                tile.connectingNodes[south] = startNode;
             } else if (tile.x > startNode.x) {
-                return startNode.childNodes[east] = tile;
+                startNode.connectingNodes[east] = tile;
+                tile.connectingNodes[west] = startNode;
             } else if (tile.y > startNode.y) {
-                return startNode.childNodes[south] = tile;
-            } else {
-                return startNode.childNodes[west] = tile;
+                startNode.connectingNodes[south] = tile;
+                tile.connectingNodes[north] = startNode;
+            } else if (tile.x < startNode.x) {
+                startNode.connectingNodes[west] = tile;
+                tile.connectingNodes[east] = startNode;
             }
         }
-    }
-
-    deleteChildNode(grid, i, parent) {
-        let tile = grid[i];
-
-        return parent.childNodes[parent.childNodes.indexOf(tile)] = undefined;
     }
 
     isConnected(node1, grid, i) {
         let lsnLength = node1.length;
         return (
-            node1[lsnLength - 1].childNodes[0] === grid[i] ||
-            node1[lsnLength - 1].childNodes[1] === grid[i] ||
-            node1[lsnLength - 1].childNodes[2] === grid[i] ||
-            node1[lsnLength - 1].childNodes[3] === grid[i]
+            node1[lsnLength - 1].connectingNodes[0] === grid[i] ||
+            node1[lsnLength - 1].connectingNodes[1] === grid[i] ||
+            node1[lsnLength - 1].connectingNodes[2] === grid[i] ||
+            node1[lsnLength - 1].connectingNodes[3] === grid[i]
         );
     }
 
-    checkForchildNodes(grid, i) {
+
+    checkForConnectingNodes(tile) {
+        console.log(this.startNode)
         let north = 0;
         let east = 1;
         let south = 2;
         let west = 3;
-
-        return grid[i].childNodes[north] === undefined && grid[i].childNodes[east] === undefined && grid[i].childNodes[south] === undefined && grid[i].childNodes[west] === undefined;
+        return (tile.connectingNodes[north] === undefined && tile.connectingNodes[east] === undefined && tile.connectingNodes[south] === undefined && tile.connectingNodes[west] === undefined && this.startNode !== tile);
     }
 
     checkNumberOfCnNodes(grid, i) {
         let count = 0;
-
-        for (let j = 0; j < grid[i].childNodes.length; j++) {
-            if (grid[i].childNodes[j] !== undefined) {
+        for (let j = 0; j < grid[i].connectingNodes.length; j++) {
+            if (grid[i].connectingNodes[j] !== undefined) {
                 count++;
             }
         }
         return count;
-    }
-
-    setStartNode(grid, i) {
-        let StNode = this.startNode;
-        let tile = grid[i];
-
-        if (StNode === tile && tile.startNode === false) {
-            tile.startNode = true;
-        } else if (tile.startNode === true) {
-            StNode = undefined;
-            tile.startNode = false;
-        }
     }
 
     flipNode(grid, i) {
@@ -280,21 +243,19 @@ export class TileGrid {
     }
 
     updatePath() {
-        let paths = this.paths;
         let tiles = this.tiles;
         let grid = this.tileGrid;
         let nodes = this.nodes;
+        let paths = this.paths;
         let north = 0;
         let east = 1;
         let south = 2;
         let west = 3;
         if (nodes.length > 1) {
             for (let i = 0; i < nodes.length; i++) {
-                if (nodes[i].startNode) {
-                    for (let j = 0; j < nodes[i].childNodes.length; j++) {
-                        if (nodes[i].childNodes[j] !== undefined) {
-                            createPath(j, grid, nodes, i, tiles, north, south, east, west, paths);
-                        }
+                for (let j = 0; j < nodes[i].connectingNodes.length; j++) {
+                    if (nodes[i].connectingNodes[j] !== undefined) {
+                        createPath(j, grid, nodes, i, tiles, north, south, east, west, paths);
                     }
                 }
             }
@@ -306,25 +267,25 @@ function createPath(j, grid, nodes, i, tiles, north, south, east, west, paths) {
     let path = [];
     switch (j) {
         case (north):
-            for (let k = grid.indexOf(nodes[i]); k >= grid.indexOf(nodes[i].childNodes[j]); k -= Math.sqrt(tiles)) {
+            for (let k = grid.indexOf(nodes[i]); k >= grid.indexOf(nodes[i].connectingNodes[j]); k -= Math.sqrt(tiles)) {
                 grid[k].traversable = true;
                 path.push(grid[k]);
             }
             break;
         case (south):
-            for (let k = grid.indexOf(nodes[i]); k <= grid.indexOf(nodes[i].childNodes[j]); k += Math.sqrt(tiles)) {
+            for (let k = grid.indexOf(nodes[i]); k <= grid.indexOf(nodes[i].connectingNodes[j]); k += Math.sqrt(tiles)) {
                 grid[k].traversable = true;
                 path.push(grid[k]);
             }
             break;
         case (east):
-            for (let k = grid.indexOf(nodes[i]); k <= grid.indexOf(nodes[i].childNodes[j]); k++) {
+            for (let k = grid.indexOf(nodes[i]); k <= grid.indexOf(nodes[i].connectingNodes[j]); k++) {
                 grid[k].traversable = true;
                 path.push(grid[k]);
             }
             break;
         case (west):
-            for (let k = grid.indexOf(nodes[i]); k >= grid.indexOf(nodes[i].childNodes[j]); k--) {
+            for (let k = grid.indexOf(nodes[i]); k >= grid.indexOf(nodes[i].connectingNodes[j]); k--) {
                 grid[k].traversable = true;
                 path.push(grid[k]);
             }
@@ -332,9 +293,6 @@ function createPath(j, grid, nodes, i, tiles, north, south, east, west, paths) {
         default:
             break;
     }
-
-    path[0].endPoint = true;
-    path[path.length-1].endPoint = true;
     paths.push(path);
 }
 
--- pathfinding/scripts/app.js
+++ pathfinding/scripts/app.js
@@ -1,5 +1,4 @@
 import { TileGrid } from "./TileGrid.js";
-import { HelperFunctions } from "./helperFunctions.js";
 import { InputHelper } from "./inputHelper.js";
 
 export const canvasElem = document.getElementById('canvas');
@@ -7,15 +6,13 @@ const tileGrid = new TileGrid(1024);
 const inputHelper = new InputHelper();
 
 let prevTime;
+
 export let keyArr = [];
-
-
 
 tileGrid.createTileGrid();
 
 window.addEventListener("keydown", inputHelper.keyDownEventsHandler);
 window.addEventListener("keyup", inputHelper.keyUpEventsHandler);
-
 canvasElem.addEventListener('mousedown', function(e) {
     getCursorPosition(canvasElem, e)
 })
@@ -26,22 +23,16 @@ function getCursorPosition(canvasElem, event) {
     const y = event.clientY - rect.top
     console.clear();
     tileGrid.createNode(x, y);
-    console.log(tileGrid.pathArray)
 }
-
 
 requestAnimationFrame(gameLoop);
 
-
 function gameLoop(now) {
     let dt = getDelta(now);
-
     update(dt);
     draw(dt);
-
     requestAnimationFrame(gameLoop);
 }
-
 
 function update(dt) {
     
@@ -53,18 +44,10 @@ function physics(dt) {
 
 function draw() {
     const ctx = canvasElem.getContext('2d');
-
     ctx.clearRect(0, 0, 600, 480);
     ctx.fillStyle = "gray";
     ctx.fillRect(0, 0, canvasElem.width, canvasElem.height);
-
     tileGrid.drawTileGrid(ctx);
-}
-
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
 }
 
 function getDelta(now) {
