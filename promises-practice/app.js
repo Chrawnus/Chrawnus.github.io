07b3ad1f351e1523b140810d81921c0c313dbcfb
Chrawnus <65030578+Chrawnus@users.noreply.github.com>
2021-03-09 21:00:13 +0200

    Update app.js

--- promises-practice/app.js
+++ promises-practice/app.js
@@ -1,11 +1,17 @@
-let promise = fetch('./assets/images/coffee.jpg');
-
-
-let promise2 = promise.then(response => {
+fetch('./assets/images/coffee.jpg')
+.then(response => {
     if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
     } else {
         return response.blob();
     }
-});
-
+})
+.then(myBlob => {
+    let objectURL = URL.createObjectURL(myBlob);
+    let image = document.createElement('img');
+    image.src = objectURL;
+    document.body.appendChild(image);
+})
+.catch(e => {
+    console.log('There has been a problem with your fetch operation: ' + e.message);
+})
