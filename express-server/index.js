91eaeb60250daafdb140ce6cb163ade702c54fea
Chrawnus <65030578+Chrawnus@users.noreply.github.com>
2021-03-17 12:40:03 +0200

    fetch and stuff
    -

---
+++ express-server/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const expressJSONOptions = { 
+    limit: '1mb'};
+
+const expressURLEncodedOptions = {
+    extended: false 
+}
+
+app.use(express.static('public'));
+app.use(express.json(expressJSONOptions));
+app.use(express.urlencoded(expressURLEncodedOptions));
+
+app.listen(port, () => console.log(`listening at port ${port}`));
+
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.status(201).send('Created User');
+});
+
+
+
+app.post('/api', (req, res) => {
+    console.log(req.body);
+    const data = req.body;
+    res.json({
+        status: 'success',
+        latitude: data.lat,
+        longitude: data.lon
+    }); 
+});
---
+++ express-server/public/scripts/app.js
@@ -0,0 +1,45 @@
+
+if ('geolocation' in navigator) {
+    console.log('geolocation available');
+    navigator.geolocation.getCurrentPosition(async position => {
+        const { latElem, lonElem } = getDomElements();
+        const { lat, lon } = getPositionVariables(position);
+        displayPositionInDOM(latElem, lat, lonElem, lon);
+        console.log(position); 
+
+        await fetchLocationData(lat, lon);
+    });
+} else {
+    console.log('geolocation not available');
+}
+
+function getDomElements() {
+    const latElem = document.querySelector('#latitude');
+    const lonElem = document.querySelector('#longitude');
+    return { latElem, lonElem };
+}
+
+function getPositionVariables(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    return { lat, lon };
+}
+
+function displayPositionInDOM(latElem, lat, lonElem, lon) {
+    latElem.textContent = lat;
+    lonElem.textContent = lon;
+}
+
+async function fetchLocationData(lat, lon) {
+    const data = { lat, lon };
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    };
+    const response = await fetch('api', options);
+    const jsonData = await response.json();
+    console.log(jsonData);
+}
