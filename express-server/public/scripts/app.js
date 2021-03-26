52c8669b153d1e26736ac117b812c5586af16b8e
Chrawnus <65030578+Chrawnus@users.noreply.github.com>
2021-03-26 11:24:10 +0200

    Update app.js

--- express-server/public/scripts/app.js
+++ express-server/public/scripts/app.js
@@ -5,33 +5,32 @@
     image.src = URL.createObjectURL(e.target.files[0]);
 }; */
 
-const { nameElem, sourceElem, descriptionElem, submitElem } = getDomElements(); 
+const { nameElem, sourceElem, descriptionElem, submitElem, imageFormElem } = getDomElements(); 
 
-submitElem.addEventListener('click', () => {
+imageFormElem.addEventListener('submit', () => {
     const name = nameElem.value; 
     const source = sourceElem.value; 
     const description = descriptionElem.value;
     
     const data = { name, source, description }
 
-    console.log("data");
+    console.log(data);
 
     const options = {
         method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
         body: data
     };
     fetch('api', options);
+    
 })
 
 function getDomElements() {
     const nameElem = document.querySelector('#name');
     const sourceElem = document.querySelector('#source');
     const descriptionElem = document.querySelector('#description');
+    const imageFormElem = document.querySelector('#image-form');
     const submitElem = document.querySelector('#submit-button')
-    return { nameElem, sourceElem, descriptionElem, submitElem };
+    return { nameElem, sourceElem, descriptionElem, submitElem, imageFormElem };
 }
 
 
