ebba41eec2e34f5f5f32255e43a0ac442487f4e8
Chrawnus <65030578+Chrawnus@users.noreply.github.com>
2021-03-26 10:37:29 +0200

    -

--- express-server/index.js
+++ express-server/index.js
@@ -1,6 +1,16 @@
+const util = require('util');
+const fs = require('fs');
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const port = 3000;
+
+
+// read JSON file and convert to javascript-code
+let info = fs.readFileSync('./data/info.json');
+let processed = JSON.parse(info);
+
+
 
 const expressJSONOptions = { 
     limit: '1mb'};
@@ -23,4 +33,21 @@ app.post('/api', (req, res) => {
         latitude: data.lat,
         longitude: data.lon
     }); 
+});
+
+// POST method route
+app.post('/', (req, res) => {
+    res.send('POST request to the homepage')
+    console.log(res)
+  })
+
+
+let processedJSON = JSON.stringify(processed, null, 4);
+
+fs.writeFile('./data/info.json', processedJSON, err => {
+    if (err) {
+        console.log('Error writing file', err);
+    } else {
+        console.log('Successfully wrote file');
+    }
 });
--- express-server/public/scripts/app.js
+++ express-server/public/scripts/app.js
@@ -1,44 +1,28 @@
-if ('geolocation' in navigator) {
-    console.log('geolocation available');
-    navigator.geolocation.getCurrentPosition(async position => {
-        const { latElem, lonElem } = getDomElements();
-        const { lat, lon } = getPositionVariables(position);
-        displayPositionInDOM(latElem, lat, lonElem, lon);
-        console.log(position); 
+//const reader = new FileReader();
 
-        await fetchLocationData(lat, lon);
-    });
-} else {
-    console.log('geolocation not available');
-}
+/* const loadFile = function(e) {	
+    console.log(e.target.files)
+    image.src = URL.createObjectURL(e.target.files[0]);
+}; */
+
 
 function getDomElements() {
-    const latElem = document.querySelector('#latitude');
-    const lonElem = document.querySelector('#longitude');
-    return { latElem, lonElem };
+    const nameElem = document.querySelector('#name');
+    const sourceElem = document.querySelector('#source');
+    const descriptionElem = document.querySelector('#description');
+    return { nameElem, sourceElem, descriptionElem };
 }
 
-function getPositionVariables(position) {
-    const lat = position.coords.latitude;
-    const lon = position.coords.longitude;
-    return { lat, lon };
+function getData() {
+    const name = nameElem.textContent;
+    const source = sourceElem.textContent;
+    const description = descriptionElem.textContent;
 }
 
-function displayPositionInDOM(latElem, lat, lonElem, lon) {
-    latElem.textContent = lat;
-    lonElem.textContent = lon;
-}
 
-async function fetchLocationData(lat, lon) {
-    const data = { lat, lon };
-    const options = {
-        method: 'POST',
-        headers: {
-            'Content-type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    };
-    const response = await fetch('api', options);
-    const jsonData = await response.json();
-    console.log(jsonData);
-}
+
+const options = {
+    method: 'POST'
+};
+fetch('api', options);
+
