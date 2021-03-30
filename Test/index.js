8dcca9882852d6d6b1d50bdd07396532b9ae934d
Chrawnus <65030578+Chrawnus@users.noreply.github.com>
2021-03-30 12:48:18 +0300

    -

---
+++ Test/index.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const app = express()
+
+app.use(express.static("public")) //mappen som du lägger in index.html, style.css o.s.v.
+
+app.get('/data', function(req, res){
+  const Name = req.query.namn
+  res.end(Name) 
+})
+
+app.listen(3000, function(req, res) {
+  console.log("Uppkopplad på port 3000");
+})
