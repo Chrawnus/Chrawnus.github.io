e7908e2502f8b1d953fafcbf03e3b8e1013fb17e
Chrawnus <65030578+Chrawnus@users.noreply.github.com>
2021-04-09 10:48:58 +0300

    -

--- matter.js/src/engine.js
+++ matter.js/src/engine.js
@@ -1,31 +1,39 @@
 import { Helper } from "../HelperFunctions.js";
 import { getKey, keyCodes } from "../input.js";
+import { createCanvas, render } from "./renderer.js";
 
 
+createCanvas(600, 800);
+export const context = document.querySelector('canvas').getContext('2d');
 
-
-const Engine = Matter.Engine,
-    Render = Matter.Render,
+export const Engine = Matter.Engine,
+/*     Render = Matter.Render, */
     World = Matter.World,
     Bodies = Matter.Bodies,
-    Body = Matter.Body;
+    Body = Matter.Body,
+    Composite = Matter.Composite;
 
-const engine = Engine.create();
+export const engine = Engine.create();
 
-const render = Render.create({
+
+
+
+/* const render = Render.create({
     element: document.body,
     engine: engine
-})
+}) */
 
 
-let ground = Bodies.rectangle(400, 610, 81000, 60, { isStatic: true });
-let box1 = Bodies.trapezoid(450, 200, 60, 60, 0.5);
-let circle1 = Bodies.circle(450, 50, 15);
-let circle2 = Bodies.circle(450, 50, 15);
-let circle3 = Bodies.circle(450, 50, 15);
-let circle4 = Bodies.circle(450, 50, 15);
-let circle5 = Bodies.circle(450, 50, 15);
-let circle6 = Bodies.circle(450, 50, 15);
+export let ground = Bodies.rectangle(400, 610, 81000, 60, { isStatic: true }),
+box1 = Bodies.trapezoid(450, 200, 60, 60, 0.5),
+circle1 = Bodies.circle(450, 10, 15),
+circle2 = Bodies.circle(450, 20, 15),
+circle3 = Bodies.circle(450, 30, 15),
+circle4 = Bodies.circle(450, 40, 15),
+circle5 = Bodies.circle(450, 50, 15),
+circle6 = Bodies.circle(450, 60, 15);
+
+
 
 function playerUpdate() {
     if (circle1.velocity.x > -3) {
@@ -40,24 +48,26 @@ function playerUpdate() {
 }
 
 const bodyArr = [circle1, box1];
-const canvas = document.querySelector('canvas')
+
+export const canvas = document.querySelector('canvas')
 
 
-let mouse = Matter.Mouse.create(render.canvas);
+let mouse = Matter.Mouse.create(canvas); 
 let mouseConstraint = Matter.MouseConstraint.create(engine, {
   mouse: mouse,
   constraint: {
     render: {visible: false}
   }
 });
-render.mouse = mouse;
+//render.mouse = mouse;
 
 World.add(engine.world, [box1, ground, circle1, circle2, circle3, circle4, circle5, circle6, mouseConstraint]);
-Render.run(render);
+//Render.run(render);
 
 function gameLoop() {
     playerUpdate();
     Engine.update(engine);
+    render();
     requestAnimationFrame(gameLoop);
 }
 
@@ -66,9 +76,3 @@ function gameLoop() {
 
 requestAnimationFrame(gameLoop);
 
-
-
-
-window.addEventListener('keydown', function (e) {
-
-});
