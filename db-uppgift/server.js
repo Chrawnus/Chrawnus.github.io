30733678ed58a643f762b1d161bb598dcab3f966
Chrawnus <65030578+Chrawnus@users.noreply.github.com>
2021-04-21 14:26:29 +0300

    -

--- db-uppgift/server.js
+++ db-uppgift/server.js
@@ -1,4 +1,4 @@
-const { response } = require('express');
+const { request } = require('express');
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 
@@ -7,26 +7,62 @@ const db = new sqlite3.Database('db-uppgift.db');
 const app = express();
 
 app.set('view engine', 'ejs');
-
 app.use(express.static('public'));
+app.use(express.urlencoded({
+  extended: true
+}));
 
-app.get('/test', (req, res) => {
-    db.all(
-        "SELECT * FROM Companies", 
-        
-        (error, rows) => {
-            res.render('index', {
-                companies: rows
-            });
-/*             let html = "<html><body>";
-            for(let i = 0; i < rows.length; i++) {
-              html += rows[i].Name + ", " + rows[i].Country;
-              html += '<br>';
-            }
-            html += "</body></html>" 
-            res.send(html);*/
+app.get('/', (request, response) => {
+    // Följande sql-kommando hämtar data från alla kolumner (*) från tabellen companies. 
+    let sql = "SELECT * FROM companies";
+    db.all(sql, (error, rows) => {
+
+        response.render('index', {
+            companies: rows
         });
-    //res.send("Hello World!");
+    });
 });
+
+app.get('/game/:companyId', (request, response) => {
+    // Följande sql-kommando hämtar data från alla kolumner (*) från tabellen games 
+    // var kolumnen companyId matchar request.params.companyId
+    let sql = "SELECT * FROM games WHERE companyId = " + request.params.companyId;
+    db.all(sql, (error, rows) => {
+        response.render('games', {
+            games: rows,
+            companyId: request.params.companyId
+        });
+    });
+});
+
+app.post('/game/:companyId', (request, response) => {
+    const name = request.body.name;
+    const companyId = request.params.companyId;
+    const sql = `
+    INSERT INTO games(companyId, name)
+    VALUES(${companyId}, '${name}')
+    `;
+
+    db.run(sql, () => {
+        response.redirect('/game/' + companyId);
+        response.end();
+    });
+});
+
+app.post('/game/:companyId/update/', (request, response) => {
+    const name = request.body.name;
+    const id = request.body.id;
+    const companyId = request.params.companyId;
+    const sql = `
+      UPDATE games SET
+        name = '${name}'
+      WHERE id = ${id}
+    `;
+    db.run(sql, () => {
+      response.redirect('/game/' + companyId);
+      response.end();
+    });
+  });
+
 
 app.listen(3000);
