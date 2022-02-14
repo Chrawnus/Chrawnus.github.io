1248a5ca6faa5250eb9576742feb3ec51d9528bb
Chrawnus <65030578+Chrawnus@users.noreply.github.com>
2022-02-14 22:22:04 +0200

    styling and stuff

--- infini-wisp-lookup/app.js
+++ infini-wisp-lookup/app.js
@@ -16,19 +16,23 @@ xhttp.send();
 function myFunction(xml) {
     const names = ["Reduce Lifetime", "Chain Spell", "Ping-pong Path/Orbiting Arc", "Spiral Arc", "Phasing"];
     const mods = [-42, -30, 25, 75, 80];
-    let reqModPermutations = JSON.parse(xml.response);
+    const reqModPermutations = JSON.parse(xml.response);
 
     const lifetime = Number.parseInt(window.prompt("Enter lifetime of projectile"));
     const target_lifetime_add = ((-1) * lifetime - 1);
 
-    let validPermutations = reqModPermutations.filter(permutation => (permutation[0] * mods[0] + permutation[1] * mods[1] + permutation[2] * mods[2] + permutation[3] * mods[3] + permutation[4] * mods[4] === target_lifetime_add));
+    const validPermutations = reqModPermutations.filter(permutation => (permutation[0] * mods[0] + permutation[1] * mods[1] + permutation[2] * mods[2] + permutation[3] * mods[3] + permutation[4] * mods[4] === target_lifetime_add));
 
     const totalModifiers = Number.parseInt(window.prompt(`${validPermutations.length} valid permutations found, please enter a desired max amount of total modifiers`));
 
-    let acceptablePermutations = validPermutations.filter(permutation => (permutation[0] + permutation[1] + permutation[2] + permutation[3] + permutation[4] <= totalModifiers));
+    const acceptablePermutations = validPermutations.filter(permutation => (permutation[0] + permutation[1] + permutation[2] + permutation[3] + permutation[4] <= totalModifiers));
 
-    for (let i = 0; i < acceptablePermutations.length; i++) {
-        const p = acceptablePermutations[i];
+    const sortedAcceptablePermutations = acceptablePermutations.sort(function (a, b) {
+        return ((a[0] * mods[0] + a[1] * mods[1] + a[2] * mods[2] + a[3] * mods[3] + a[4] * mods[4]) - b[0] * mods[0] + b[1] * mods[1] + b[2] * mods[2] + b[3] * mods[3] + b[4] * mods[4]);
+    });
+
+    for (let i = 0; i < sortedAcceptablePermutations.length; i++) {
+        const p = sortedAcceptablePermutations[i];
         paragraph.innerHTML = paragraph.innerHTML + `<img src="./img/Spell_lifetime_down.png" alt="${names[0]}"> x ${p[0]}, <img src="./img/Spell_chain_shot.png" alt="${names[1]}"> x ${p[1]}, <img src="./img/Spell_orbit_shot.png" alt="${names[2]}"> x ${p[2]}, <img src="./img/Spell_lifetime.png" alt="${names[3]}"> x ${p[3]}, <img src="./img/Spell_phasing_arc.png" alt="${names[4]}"> x ${p[4]}, total: ${p[0] + p[1] + p[2] + p[3] + p[4]} <br>`;
     }
 }
