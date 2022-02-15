684d60e9cd0101d7f8ab23400a88a9154f2a9326
Chrawnus <65030578+Chrawnus@users.noreply.github.com>
2022-02-15 18:01:30 +0200

    rebuilt JSON into arrays of objects to hopefully make look up faster

--- infini-wisp-lookup/app.js
+++ infini-wisp-lookup/app.js
@@ -15,38 +15,34 @@ function myFunction(xml) {
     const names = ["Reduce Lifetime", "Chain Spell", "Ping-pong Path/Orbiting Arc", "Spiral Arc", "Phasing"];
     const mods = [-42, -30, 25, 75, 80];
     const reqModPermutations = JSON.parse(xml.response);
+    
     const lifetime = Number.parseInt(window.prompt("Enter lifetime of projectile"));
     const target_lifetime_add = ((-1) * lifetime - 1);
+    
+    const validPermutations = reqModPermutations.filter(permutation => (permutation[0]["lifetime_add"] === target_lifetime_add)).flat();
+    
+    const totalModifiers = Number.parseInt(window.prompt(`${validPermutations.length} valid permutations found, please enter a desired amount of permutations`));
 
-    const validPermutations = reqModPermutations.filter(permutation => (permutation[0] * mods[0] + permutation[1] * mods[1] + permutation[2] * mods[2] + permutation[3] * mods[3] + permutation[4] * mods[4] === target_lifetime_add));
-
-    const totalModifiers = Number.parseInt(window.prompt(`${validPermutations.length} valid permutations found, please enter a desired max amount of total modifiers`));
-
-    const acceptablePermutations = validPermutations.filter(p => (p[0] + p[1] + p[2] + p[3] + p[4] <= totalModifiers));
-
-    const sortedAcceptablePermutations = acceptablePermutations.sort(function (a, b) {
-        return (b[0] * mods[0] + b[1] * mods[1] + b[2] * mods[2] + b[3] * mods[3] + b[4] * mods[4]) - (a[0] * mods[0] + a[1] * mods[1] + a[2] * mods[2] + a[3] * mods[3] + a[4] * mods[4]);
-    });
-
+    const acceptablePermutations = validPermutations.slice(0, totalModifiers);
+    console.log(acceptablePermutations)
     printPermutations();
 
     function printPermutations() {
         const asyncPrint = p =>
             new Promise(resolve =>
                 setTimeout(
-                    () => resolve(printArray(p)),
-                    Math.floor(Math.random() * 500)
+                    () => resolve(printArray(p)), 500
                 )
             );
-        for (let i = 0; i < sortedAcceptablePermutations.length; i++) {
-            const p = sortedAcceptablePermutations[i];
+        for (let i = 0; i < acceptablePermutations.length; i++) {
+            const p = acceptablePermutations[i];
             asyncPrint(p)
             
         }
 
 
         function printArray(p) {
-            paragraph.innerHTML = paragraph.innerHTML + `<img src="./img/Spell_lifetime_down.png" alt="${names[0]}"> x ${p[0]}, <img src="./img/Spell_chain_shot.png" alt="${names[1]}"> x ${p[1]}, <img src="./img/Spell_orbit_shot.png" alt="${names[2]}"> x ${p[2]}, <img src="./img/Spell_lifetime.png" alt="${names[3]}"> x ${p[3]}, <img src="./img/Spell_phasing_arc.png" alt="${names[4]}"> x ${p[4]}, total: ${p[0] + p[1] + p[2] + p[3] + p[4]} <br>`;
+            paragraph.innerHTML = paragraph.innerHTML + `<img src="./img/Spell_lifetime_down.png" alt="${names[0]}"> x ${p["Reduce Lifetime"]}, <img src="./img/Spell_chain_shot.png" alt="${names[1]}"> x ${p["Chain Spell"]}, <img src="./img/Spell_orbit_shot.png" alt="${names[2]}"> x ${p["Pingpong Path"]}, <img src="./img/Spell_lifetime.png" alt="${names[3]}"> x ${p["Increase Lifetime"]}, <img src="./img/Spell_phasing_arc.png" alt="${names[4]}"> x ${p["Phasing"]}, total: ${p["Total"]} <br>`;
         }
     }
 }
@@ -54,14 +50,16 @@ function myFunction(xml) {
 
 
 
-/* 
-let reqMods = [0, 0, 0, 0, 0];
-let maxCount = 31;
-let maxPpCount = 3;
-let maxIncreaseCount = 16;
-let maxPhasingCount = 16;
 
+/* const reqMods = [0, 0, 0, 0, 0];
+const maxCount = 31;
+const maxPpCount = 3;
+const maxIncreaseCount = 16;
+const maxPhasingCount = 16;
+const mods = [-42, -30, 25, 75, 80];
+const reqModPermutations = [];
 
+console.log("building initial array of objects");
 for (let i = 0; i < maxCount; i++) {
     reqMods[0] = i;
     for (let j = 0; j < maxCount; j++) {
@@ -72,29 +70,52 @@ for (let i = 0; i < maxCount; i++) {
                 reqMods[3] = l;
                 for (let m = 0; m < maxPhasingCount; m++) {
                     reqMods[4] = m;
-                    reqModPermutations.push([i, j, k, l, m]);
+                    const lifetime_add = i * mods[0] + j * mods[1] + k * mods[2] + l * mods[3] + m * mods[4];
+                    const total = i + j + k + l + m;
+                    reqModPermutations.push({
+                        "Reduce Lifetime": i,
+                        "Chain Spell": j,
+                        "Pingpong Path": k,
+                        "Increase Lifetime": l,
+                        "Phasing": m,
+                        "Total": total,
+                        "lifetime_add": lifetime_add,
+                    });
                 }
             }
         }
     }
 }
 
+console.log("Removing positive lifetime_add");
 for (let i = 0; i < reqModPermutations.length; i++) {
     const element = reqModPermutations[i];
-    if (element[0] * mods[0] + element[1] * mods[1] + element[2] * mods[2] + element[3] * mods[3] + element[4] * mods[4] >= 0) {
+    if (element["lifetime_add"] >= 0) {
         reqModPermutations.splice(i, 1);
         i--;
     }
 
 }
 
-
+console.log("Sorting permutations by lifetime_add");
 let sortedReqModPermutations = reqModPermutations.sort(function (a, b) {
-    return (b[0] * mods[0] + b[1] * mods[1] + b[2] * mods[2] + b[3] * mods[3] + b[4] + mods[4]) - (a[0] * mods[0] + a[1] * mods[1] + a[2] * mods[2] + a[3] * mods[3] + a[4] + mods[4]);
+    return b["lifetime_add"] - a["lifetime_add"];
 });
 
 
-console.log(sortedReqModPermutations); */
+console.log("chunking array");
+let chunkedSortedPermutations = []
+let i = 0;
+while (sortedReqModPermutations.length > 0) {
+    chunkedSortedPermutations.push(sortedReqModPermutations.filter(p => p["lifetime_add"] === sortedReqModPermutations[0]["lifetime_add"]).sort(function (a, b) {
+        return a["Total"] - b["Total"];
+    }));
+    sortedReqModPermutations.splice(0, chunkedSortedPermutations[chunkedSortedPermutations.length-1].length);
+    i++;
+
+}
+
+console.log(chunkedSortedPermutations); */
 
 
 
