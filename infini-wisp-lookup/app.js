e961fae872a877f07e71cb66a9449421656f59aa
Chrawnus <65030578+Chrawnus@users.noreply.github.com>
2022-02-15 21:43:18 +0200

    optimization

--- infini-wisp-lookup/app.js
+++ infini-wisp-lookup/app.js
@@ -1,31 +1,107 @@
 const paragraph = document.querySelector("#info");
+const lifetimeInputField = document.querySelector("#lifetime");
+const limitInputField = document.querySelector("#limit");
+const varianceInputField = document.querySelector("#variance");
+const lookupButton = document.querySelector("#lookup");
 
+const names = ["Reduce Lifetime", "Chain Spell", "Ping-pong Path/Orbiting Arc", "Spiral Arc", "Phasing"];
 
-const JSONFile = "reqModPermutations.json";
+const defaultInputValues = {
+    "lifetime": 40,
+    "variance": 7,
+    "limit": 5
+}
+
 var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-        myFunction(this);
+lookupButton.addEventListener("click", () => {
+    const JSONFile = "reqModPermutations.json";
+
+
+    let lifetime = Number.parseInt(lifetimeInputField.value);
+    let variance = Number.parseInt(varianceInputField.value);
+    let limit = Number.parseInt(limitInputField.value);
+
+    // compare variables with themselves to check for NaN, and replace with default input values if true;
+
+    if (lifetime !== lifetime)
+    
+        lifetime = defaultInputValues.lifetime;
+        
+    if (variance !== variance) {
+        variance = defaultInputValues.variance;
     }
-};
-xhttp.open("GET", JSONFile, true);
-xhttp.send();
 
-function myFunction(xml) {
-    const names = ["Reduce Lifetime", "Chain Spell", "Ping-pong Path/Orbiting Arc", "Spiral Arc", "Phasing"];
-    const mods = [-42, -30, 25, 75, 80];
+    if (limit !== limit) {
+        limit = defaultInputValues.limit;
+    }
+
+
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+
+            myFunction(this, lifetime, variance, limit);
+        }
+    };
+    xhttp.open("GET", JSONFile, true);
+    xhttp.send();
+})
+
+
+
+
+
+function myFunction(xml, lifetimeInput, varianceInput, limitInput) {
+
+    paragraph.innerHTML = "";
+
+    const lifetime = lifetimeInput;
+    const totalModifiers = limitInput;
+    const variance = varianceInput;
+    let target_lifetime_add;
+
+    checkIfVariance();
+
+
     const reqModPermutations = JSON.parse(xml.response);
-    
-    const lifetime = Number.parseInt(window.prompt("Enter lifetime of projectile"));
-    const target_lifetime_add = ((-1) * lifetime - 1);
-    
-    const validPermutations = reqModPermutations.filter(permutation => (permutation[0]["lifetime_add"] === target_lifetime_add)).flat();
-    
-    const totalModifiers = Number.parseInt(window.prompt(`${validPermutations.length} valid permutations found, please enter a desired amount of permutations`));
 
-    const acceptablePermutations = validPermutations.slice(0, totalModifiers);
-    console.log(acceptablePermutations)
+    let acceptablePermutations;
+    getValidPermutations();
+
     printPermutations();
+
+    function getValidPermutations() {
+        let validPermutations = [];
+        if (target_lifetime_add.length > 1) {
+            
+            for (let i = 0; i < target_lifetime_add.length; i++) {
+                const lifetime_add = target_lifetime_add[i];
+
+                validPermutations.push(...reqModPermutations.filter(permutation => (permutation[0]["lifetime_add"] === lifetime_add)).flat());
+
+            }
+            validPermutations.sort(function (a, b) {
+                return a["Total"] - b["Total"];
+            })
+            acceptablePermutations = validPermutations.slice(0, totalModifiers);
+        } else {
+            validPermutations = reqModPermutations.filter(permutation => (permutation[0]["lifetime_add"] === target_lifetime_add)).flat();
+            acceptablePermutations = validPermutations.slice(0, totalModifiers);
+        }
+        return validPermutations;
+    }
+
+    function checkIfVariance() {
+        if (variance <= 0) {
+            target_lifetime_add = ((-1) * lifetime - 1);
+        } else {
+            target_lifetime_add = [];
+            for (let i = -variance; i < variance; i++) {
+                const newLifetime = lifetime + i
+                target_lifetime_add.push(((-1) * newLifetime));
+            }
+        }
+    }
 
     function printPermutations() {
         const asyncPrint = p =>
@@ -34,15 +110,16 @@ function myFunction(xml) {
                     () => resolve(printArray(p)), 500
                 )
             );
+        
         for (let i = 0; i < acceptablePermutations.length; i++) {
             const p = acceptablePermutations[i];
             asyncPrint(p)
-            
+
         }
 
 
         function printArray(p) {
-            paragraph.innerHTML = paragraph.innerHTML + `<img src="./img/Spell_lifetime_down.png" alt="${names[0]}"> x ${p["Reduce Lifetime"]}, <img src="./img/Spell_chain_shot.png" alt="${names[1]}"> x ${p["Chain Spell"]}, <img src="./img/Spell_orbit_shot.png" alt="${names[2]}"> x ${p["Pingpong Path"]}, <img src="./img/Spell_lifetime.png" alt="${names[3]}"> x ${p["Increase Lifetime"]}, <img src="./img/Spell_phasing_arc.png" alt="${names[4]}"> x ${p["Phasing"]}, total: ${p["Total"]} <br>`;
+            paragraph.innerHTML = paragraph.innerHTML + `Lifetime: ${(-1) * p["lifetime_add"] - 1}, <img src="./img/Spell_lifetime_down.png" alt="${names[0]}"> x ${p["Reduce Lifetime"]}, <img src="./img/Spell_chain_shot.png" alt="${names[1]}"> x ${p["Chain Spell"]}, <img src="./img/Spell_orbit_shot.png" alt="${names[2]}"> x ${p["Pingpong Path"]}, <img src="./img/Spell_lifetime.png" alt="${names[3]}"> x ${p["Increase Lifetime"]}, <img src="./img/Spell_phasing_arc.png" alt="${names[4]}"> x ${p["Phasing"]}, total: ${p["Total"]} <br>`;
         }
     }
 }
