9a08ecd0ba90ec1eb5cbd4dee70ba4cce6fa7a88
Chrawnus <65030578+Chrawnus@users.noreply.github.com>
2022-05-04 01:09:03 +0300

    asteroids-like
    added comments to Draw class

--- asteroids-like/scripts/Draw.js
+++ asteroids-like/scripts/Draw.js
@@ -14,22 +14,21 @@ export class Draw {
 
 
     static canvasMethods = class {
-        static drawScreen(backgroundColor, engine, menu, player, entities, projectiles) {
+        static draw(backgroundColor, engine, menu, player, entities, projectiles) {
             const gameCtx = Draw.Canvas.gameCtx;
             const UICtx = Draw.Canvas.UICtx;
-            Draw.canvasMethods.clearCanvas(Draw.Canvas.UI, UICtx, "transparent")
-            if (player.lives > 0) {
-                if (engine.pauseState === false) {
-                    menu.drawUI(UICtx);
-                } else {
-                    menu.drawPauseUI(UICtx);
-                }
 
-            } else {
-                menu.drawUIGameOver(UICtx);
-            }
+            // Clear game canvas and UI canvas
+            Draw.canvasMethods.clearCanvas(Draw.Canvas.UI, UICtx, "transparent")
             Draw.canvasMethods.clearCanvas(Draw.Canvas.gameScreen, gameCtx, backgroundColor);
-            Draw.drawPlayer(gameCtx, player);
+
+            // Handle which UI to draw on canvas
+            Draw.drawUIHandler(player, engine, menu, UICtx);
+
+            // Draw player on canvas
+            Draw.drawEntity(gameCtx, player);
+
+            // Draw asteroids on canvas
             Draw.drawEntities(gameCtx, entities);
             Draw.drawProjectiles(gameCtx, projectiles);
         }
@@ -39,27 +38,55 @@ export class Draw {
             ctx.fillStyle = fillStyle;
             ctx.fillRect(0, 0, canvas.width, canvas.height);
         }
+    }
 
-        static translateOriginToEntity(ctx, entity) {
-            ctx.translate(entity.pos.x, entity.pos.y);
-        }
 
-        static rotateCanvasAroundEntity(ctx, entity) {
-            ctx.rotate(entity.rotationAngle);
+    /*
+    Handle which UI to draw on screen
+    depending on if the game is running,
+    paused, or if the player has lost all their lives.
+    */
+    static drawUIHandler(player, engine, menu, UICtx) {
+        if (player.lives > 0) {
+            if (engine.pauseState === false) {
+                menu.drawUI(UICtx);
+            } else {
+                menu.drawPauseUI(UICtx);
+            }
+        } else {
+            menu.drawUIGameOver(UICtx);
         }
     }
 
+    /*
+    Loop through a given array of entities 
+    and draw them on the screen.
+    */
     static drawEntities(ctx, entities) {
         for (let i = 0; i < entities.length; i++) {
             const entity = entities[i];
             ctx.save();
-            Draw.canvasMethods.translateOriginToEntity(ctx, entity);
-            Draw.canvasMethods.rotateCanvasAroundEntity(ctx, entity);
+            Draw.translateAndRotate(ctx, entity);
             Draw.Geometry.drawShape(ctx, entity.points, entity.strokeStyle);
             ctx.restore();
         }
     }
 
+    /*
+    Draw the a single entity
+    on the screen.
+    */
+    static drawEntity(ctx, entity) {
+        ctx.save();
+        Draw.translateAndRotate(ctx, entity);
+        Draw.Geometry.drawShape(ctx, entity.points, entity.strokeStyle);
+        ctx.restore();
+    }
+
+    /*
+    Draw all non-expired projectiles
+    on the screen. 
+    */
     static drawProjectiles(ctx, projectiles) {
         for (let i = 0; i < projectiles.length; i++) {
             const projectile = projectiles[i];
@@ -67,13 +94,9 @@ export class Draw {
         }
     }
 
-
-    static drawPlayer(ctx, player) {
-        ctx.save();
-        Draw.canvasMethods.translateOriginToEntity(ctx, player);
-        Draw.canvasMethods.rotateCanvasAroundEntity(ctx, player);
-        Draw.Geometry.drawShape(ctx, player.points, player.strokeStyle);
-        ctx.restore();
+    static translateAndRotate(ctx, entity) {
+        ctx.translate(entity.pos.x, entity.pos.y);
+        ctx.rotate(entity.rotationAngle);
     }
 
     static Geometry = class {
@@ -81,7 +104,7 @@ export class Draw {
         * Draws a shape from an array of point2D objects.
         * @param  {CanvasRenderingContext2D} ctx
         * @param  {Array} points Array from which to build the shape
-        * @param  {StrokeStyle} strokeStyle
+        * @param  strokeStyle
         */
         static drawShape(ctx, points, strokeStyle) {
             ctx.strokeStyle = strokeStyle;
@@ -99,10 +122,10 @@ export class Draw {
         /**
         * Draws a circle from a given mid point and radius.
         * @param  {CanvasRenderingContext2D} ctx
-        * @param  {StrokeStyle} strokeStyle
-        * @param  {Number} lineWidth Thickness of the circumference of the circle
-        * @param  {Point2d} center Center coordinate of the circle
-        * @param  {Number} radius Length of the circle radius
+        * @param  strokeStyle
+        * @param  {Number} lineWidth
+        * @param  {Point2d} center
+        * @param  {Number} radius
         */
         static drawCircle(ctx, strokeStyle, lineWidth, center, radius) {
             ctx.strokeStyle = strokeStyle;
